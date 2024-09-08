a57175c524cefc156087b1df917ca28f42153998
Cristian <cristianscheid@gmail.com>
2024-09-08 13:19:11 -0300

    feat(front): adjust button y margin

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ type, onClick, label }) {
     <button
       type={type}
       onClick={onClick}
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className="text-white my-4 w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
     >
       {label}
     </button>
