d29db2e00d4a6d6b6311b2e442030970a85ed8fe
Cristian <cristianscheid@gmail.com>
2024-10-05 10:27:54 -0300

    fix(front): adjust 404 page and page container element to handle it

--- frontend/src/components/PageContainer.jsx
+++ frontend/src/components/PageContainer.jsx
@@ -1,6 +1,10 @@
-export default function PageContainer({ title, children }) {
+export default function PageContainer({ title, children, is404 = false }) {
   return (
-    <div className="flex flex-col items-center justify-between h-[calc(100vh-72px)] overflow-y-auto dark:bg-gray-800 p-5">
+    <div
+      className={`flex flex-col items-center justify-between ${
+        is404 ? "h-screen" : "h-[calc(100vh-72px)]"
+      } overflow-y-auto dark:bg-gray-800 p-5`}
+    >
       <div className="flex flex-col items-center mt-5 space-y-5 text-center">
         <h1 className="text-5xl font-bold text-gray-100 mb-5">{title}</h1>
         {children}
--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import PageContainer from "../components/PageContainer";
 
 export default function NotFoundPage() {
   return (
-    <PageContainer title="404 - Page Not Found">
+    <PageContainer title="404 - Page Not Found" is404>
       <p className="text-lg text-gray-300 mb-6">
         Oops! The page you are looking for could not be found.
       </p>
@@ -10,7 +11,11 @@ export default function NotFoundPage() {
         It might be that the URL is incorrect or the page has been removed.
       </p>
       <p className="text-md text-gray-400 max-w-lg mb-4">
-        Please check the URL or return to the homepage.
+        Please check the URL or return to the{" "}
+        <Link to="/" className="text-blue-500 underline hover:text-blue-700">
+          Home Page
+        </Link>
+        .
       </p>
     </PageContainer>
   );
