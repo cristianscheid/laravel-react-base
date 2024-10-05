8015e658d076b1e29e9cfec05a7f461b2275197e
Cristian <cristianscheid@gmail.com>
2024-10-05 10:11:38 -0300

    fix(front): minor adjustments on button, input and notification components

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,9 +1,22 @@
-export default function Button({ type, onClick, label }) {
+export default function Button({
+  type = "button",
+  onClick,
+  label,
+  variant = "primary",
+}) {
+  const baseStyles =
+    "text-white my-4 w-full font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none";
+
+  const colorStyles =
+    variant === "primary"
+      ? "bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      : "bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800";
+
   return (
     <button
       type={type}
       onClick={onClick}
-      className="text-white my-4 w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className={`${baseStyles} ${colorStyles}`}
     >
       {label}
     </button>
--- frontend/src/components/ui/Notification.jsx
+++ frontend/src/components/ui/Notification.jsx
@@ -1,10 +1,15 @@
 const Notification = ({ type, messages }) => {
   if (!messages || messages.length === 0) return null;
 
+  const textColor =
+    type === "error"
+      ? "text-red-800 dark:text-red-400"
+      : "text-green-800 dark:text-green-400";
+
   return (
-    <div className="max-w-sm mx-auto p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
+    <div className="max-w-sm mx-auto p-4 mb-4 text-sm bg-gray-50 dark:bg-gray-800 rounded-lg">
       {messages.map((message, index) => (
-        <p key={index} className={type}>
+        <p key={index} className={textColor}>
           {message}
         </p>
       ))}
