af176165560da2e960c7e128a693d587915a509c
Cristian <cristianscheid@gmail.com>
2024-10-06 09:13:09 -0300

    fix(back): remove unused import and add comments on api file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'show']);
     Route::put('/user', [UserController::class, 'update']);
