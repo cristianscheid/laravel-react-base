535a6c7779a69f871dc8a1bc82475c345b329ca9
Cristian <cristianscheid@gmail.com>
2024-10-05 21:25:58 -0300

    fix(front): centralize content of not found page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -5,19 +5,21 @@ import PageContainer from "../components/PageContainer";
 export default function NotFoundPage() {
   return (
     <PageContainer title="404 - Page Not Found" is404>
-      <p className="text-lg text-gray-300 mb-6">
-        Oops! The page you are looking for could not be found.
-      </p>
-      <p className="text-md text-gray-400 max-w-lg mb-4">
-        It might be that the URL is incorrect or the page has been removed.
-      </p>
-      <p className="text-md text-gray-400 max-w-lg mb-4">
-        Please check the URL or return to the{" "}
-        <Link to="/" className="text-blue-500 underline hover:text-blue-700">
-          Home Page
-        </Link>
-        .
-      </p>
+      <div className="flex flex-col items-center justify-center mt-6 space-y-5 text-center">
+        <p className="text-lg text-gray-300 mb-6">
+          Oops! The page you are looking for could not be found.
+        </p>
+        <p className="text-md text-gray-400 max-w-lg mb-4">
+          It might be that the URL is incorrect or the page has been removed.
+        </p>
+        <p className="text-md text-gray-400 max-w-lg mb-4">
+          Please check the URL or return to the{" "}
+          <Link to="/" className="text-blue-500 underline hover:text-blue-700">
+            Home Page
+          </Link>
+          .
+        </p>
+      </div>
     </PageContainer>
   );
 }
