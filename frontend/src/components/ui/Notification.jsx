dd8a5c02b728752a295760bb399a220ad20c73c0
Cristian <cristianscheid@gmail.com>
2024-10-06 07:06:57 -0300

    fix(front): center text of notification component

--- frontend/src/components/ui/Notification.jsx
+++ frontend/src/components/ui/Notification.jsx
@@ -4,7 +4,7 @@ const Notification = ({ type, messages }) => {
   const textColor = type === "error" ? "text-red-400" : "text-green-400";
 
   return (
-    <div className="max-w-sm mx-auto p-4 mb-4 text-sm bg-gray-800 rounded-lg">
+    <div className="max-w-sm mx-auto p-4 mb-4 text-center text-sm bg-gray-800 rounded-lg">
       {messages.map((message, index) => (
         <p key={index} className={textColor}>
           {message}
